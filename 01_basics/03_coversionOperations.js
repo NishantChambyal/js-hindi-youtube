70993e395a9b469f91ace4117ba58b048b08fdad
NishantChambyal <146934202+NishantChambyal@users.noreply.github.com>
2024-09-14 10:45:36 +0000

    conversions and Operations

--- 01_basics/03_coversionOperations.js
+++ 01_basics/03_coversionOperations.js
@@ -9,31 +9,73 @@ let score = undefined;
 
 // console.log(valueInNumber);
 
-
-
 // "33" => 22
 // "33abc" => NaN
 // true => 1 , false => 0
 // undefine => NaN
 
+let isLoggedIn = 0;
 
-let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn);
 
-let booleanIsLoggedIn =  Boolean(isLoggedIn)
-
-console.log(booleanIsLoggedIn);
-console.log(typeof booleanIsLoggedIn);
-
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
 
 // 1 => true ,  0 => false
 // "" => false
 // "nishant" => true
 
+let someNumber = false;
 
-let someNumber = false
+let stringNumber = String(someNumber);
 
-let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// ********************************** operations *****************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+// console.log(typeof negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello";
+let str2 = " nishant";
+
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(((3 + 4) * 5) % 3);
+
+
+// console.log(+true);
+// console.log(+"");   
+
+let num1 , num2 , num3;
+
+num1 = num2 = num3 = 2 + 2
+
+// console.log(num1 , num2 , num3);
+
+let gameCounter = 100
+
+gameCounter++;
+++gameCounter;
+
+console.log(gameCounter);
+
+
 
