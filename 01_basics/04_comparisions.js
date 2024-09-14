836a568ad36ea406a2ae5f36ea87a4117ac19fcf
NishantChambyal <146934202+NishantChambyal@users.noreply.github.com>
2024-09-14 10:59:13 +0000

    data conversions rule

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,28 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 2);
+// console.log(2 != 2);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+
+
+// do not do this type of conversion because its confusing
+
+// === 
+
+console.log("2" === 2);
+
