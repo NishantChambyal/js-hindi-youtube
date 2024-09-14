f66833c5955ff54ac2c7eac67594ea6442504c33
NishantChambyal <146934202+NishantChambyal@users.noreply.github.com>
2024-09-14 09:57:11 +0000

    conversion

---
+++ 01_basics/03_coversionOperations.js
@@ -0,0 +1,39 @@
+let score = undefined;
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+
+
+// "33" => 22
+// "33abc" => NaN
+// true => 1 , false => 0
+// undefine => NaN
+
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn =  Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+
+// 1 => true ,  0 => false
+// "" => false
+// "nishant" => true
+
+
+let someNumber = false
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
