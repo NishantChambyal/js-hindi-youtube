458ef9b98e4f609e820118b1573b565a12360087
NishantChambyal <146934202+NishantChambyal@users.noreply.github.com>
2024-09-15 09:50:33 +0000

    arrays 1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,48 @@
+// arrays
+
+const myArr = [0,1,2,3,4,5,6, true , "nishant"]
+
+const myHeros = ["Shaktiman", "nagraj"]
+const myArr2 = new Array(1,2,3,4)
+
+
+// console.log(myArr2);
+
+
+// console.log(myArr[0]);
+// console.log(myArr[1]);
+// console.log(myArr[2]);
+
+
+//Array Methods
+
+// myArr.push(6)
+// myArr.pop()
+// myArr.shift()
+// myArr.unshift(5)
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr  = myArr.join()    // converts into string
+  
+
+
+// console.log(myArr);
+// console.log(newArr);
+
+
+// slice , splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1 , 3)   // starts from index 1 and goes till 2 not 3 because n-1
+
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3)  // starts from 1 and goes till given index because its obbey n-1 rule and orginal array will menupulate also and left inside value which is not splice
+console.log(myn2);
+console.log("c", myArr);
+
+
