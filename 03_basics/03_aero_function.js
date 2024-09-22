29a5b8f178cd22b0d592c328073e8fb7f6f8eb3a
NishantChambyal <146934202+NishantChambyal@users.noreply.github.com>
2024-09-22 05:21:04 +0000

    aero functions

--- 03_basics/03_aero_function.js
+++ 03_basics/03_aero_function.js
@@ -1,9 +1,58 @@
 const user = {
-   username: "hitesh",
-   price: 999 ,
+  userName: "nishant",
+  price: 999,
 
-   welcomeMessage: function() {
-    console.log(`${} , welcome to website`);
-    
-   }
-}
+  welcomeMessage: function () {
+    console.log(`${this.userName} welcome to website`);
+    console.log(this);
+  },
+};
+
+// user.welcomeMessage();
+
+// user.userName = "sam";
+
+// user.welcomeMessage();
+
+// console.log(this);
+
+// function chai (){
+//    let userName = "nishant"
+//    console.log(this.userName);
+
+// }
+
+// chai()
+
+// const chai  = function (){
+//    let userName = "nishant"
+//    console.log(this.userName);
+// }
+
+// chai()
+
+// const chai  =  () => {
+//    let userName = "nishant"
+//    console.log(this);
+// }
+
+// chai()
+
+// const addTwo = (num1 , num2) => {
+//    return num1 + num2
+// }
+
+// console.log(addTwo(22 , 4));
+
+// const addTwo = (num1 , num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => (num1 + num2); 
+
+const addTwo = (num1, num2) => ({userName: "nishant"}); 
+console.log(addTwo(22, 4));
+
+
+
+// const myArr = [2,5,6,4,9]
+
+// myArr.forEach()
