09d0e82a2e66155584e90637ae5dcda5e623e1ac
NishantChambyal <146934202+NishantChambyal@users.noreply.github.com>
2024-09-25 08:11:15 +0000

    resume

---
+++ 05_iterations/six.js
@@ -0,0 +1,34 @@
+// const coding =  ["js", "ruby", "java", "python", "cpp"]
+
+// const values = coding.forEach((item)=>{
+//     // console.log(item)
+//     return item                                      // in for each loop function does not return value
+    
+// })
+
+// console.log(values);
+
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNums.filter((num) => num > 4 )  // filter is return value we can store it in varriables
+// console.log(newNums);                            // implicit example no need to give return because it assume return automatically
+
+
+// const newNums = myNums.filter((num) => {
+//     return num > 4                            // filter is return value we can store it in varriables
+// } )                                           // explicit example here return is necessary
+// console.log(newNums);                                                    
+
+
+// const newNums = []
+
+// myNums.forEach((num)=>{
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// })
+
+
+// console.log(newNums);
+
